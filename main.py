8433d34b84e2f31ca98f83fc6d9424576e9837ab
emoney17 <emmanuel.million@tutanota.com>
2023-05-11 19:38:59 -0500

    tokenize lines working

--- main.py
+++ main.py
@@ -3,7 +3,40 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 def main():
-    print("hello")
+
+    # Colors
+    colorWhite = (225, 225, 225)
+    colorRed   = (225, 0, 0) 
+    colorBlue  = (0, 0, 225)
+    colorGreen = (0, 225, 0)
+
+    # Open image
+    img = Image.open("./imgs/black.jpg")
+
+    # Call draw method to add graphics to image
+    snippet = ImageDraw.Draw(img)
+
+    # Custom font and size
+    myFont = ImageFont.truetype("./font/CaskaydiaCove-Regular.ttf", 32)
+
+    myString = "#include <iostream>\n\nint main() {\n    std::cout << \"Hello world\" << std::endl;\n    return 0;\n}\n"
+
+    # Coordinates
+    x = 10
+    y = 0
+
+    # Input string
+    mySplit = myString.split('\n')
+
+    # Draw each line
+    for line in mySplit:
+        snippet.text((x, y), line, font=myFont, fill =(colorWhite))
+
+        # Move down 50 pix
+        y += 50
+
+    # Show the edit
+    img.show()
 
 if __name__ == "__main__":
     main()
